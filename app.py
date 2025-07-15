7bd8ff1bb5e41c7e7c559988b48a06df94a1ae6f
workof-ankan <officialwork.ankan@gmail.com>
2025-07-16 00:28:54 +0530

    Fix : bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -64,4 +64,7 @@ def serve_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
